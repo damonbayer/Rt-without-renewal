e1960958ccb80b3991a720a3da25e4049aec0025
Samuel Brand <48288458+SamuelBrand1@users.noreply.github.com>
2024-03-12 23:42:58 +0000

    doc string generate_observations function

--- EpiAware/src/abstract-types.jl
+++ EpiAware/src/abstract-types.jl
@@ -43,6 +43,25 @@ function generate_latent(latent_model::AbstractLatentModel, n)
     return nothing
 end
 
+@doc raw"""
+Constructor function for a observation model with case data `y_t` and unobserved infections
+`I_t`.
+
+The `generate_observations` function implements a model of generating and/or scoring
+observered cast data `y_t`. Which model for observations is implemented is set by the type
+of `observation_model`. If no implemention is defined for the type of `observation_model`,
+then `EpiAware` will pass a warning and return `nothing`.
+
+## Interface to `Turing.jl` probablilistic programming language (PPL)
+
+Apart from the no implementation fallback method, the `generate_observations` implementation
+function should return a constructor function for a
+    [`DynamicPPL.Model`](https://turinglang.org/DynamicPPL.jl/stable/api/#DynamicPPL.Model)
+object. Observed data `y_t` passed to the observation model constructor will accumulate
+log-likelihood in Bayesian inference, whereas `y_t = missing` will generate case data
+conditional on unobserved infections `I_t`. Priors for model parameters are fields of
+`observation_model`.
+"""
 function generate_observations(observation_model::AbstractObservationModel,
         y_t,
         I_t;
