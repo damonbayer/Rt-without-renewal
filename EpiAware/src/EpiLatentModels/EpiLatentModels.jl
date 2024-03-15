add2a7b86ca321fd626f805fbb8bebf2c82da031
Sam <s.e.abbott12@gmail.com>
2024-03-15 16:00:36 +0000

    first pass at thin plate spline

--- EpiAware/src/EpiLatentModels/EpiLatentModels.jl
+++ EpiAware/src/EpiLatentModels/EpiLatentModels.jl
@@ -8,11 +8,12 @@ using ..EpiAwareBase
 
 using Turing, Distributions, DocStringExtensions
 
-export RandomWalk, AR, DiffLatentModel
+export RandomWalk, AR, ThinPlateSpline, DiffLatentModel
 
 include("docstrings.jl")
 include("randomwalk.jl")
 include("autoregressive.jl")
+include("thinplatespline.jl")
 include("difflatentmodel.jl")
 include("utils.jl")
 
